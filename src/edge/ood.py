a92137876e1a14a11fefffb9ca3192692b92386a
BrunoSantome <bruno.santomeantolin@gmail.com>
2026-09-13 11:28:54 +0100

    baseline ood implementation for end-to-end run experiment

--- src/edge/ood.py
+++ src/edge/ood.py
@@ -1,1 +1,6 @@
 # decide whether a prediction is unknown. Unknown detection mechanism
+
+# Simplest ood possible, to test the pipeline ood. A high value of threshold will escalte almost every time, just
+# to test the whole end-to-end pìpeline. This must be improved with a more complex ood mechanism TODO
+def is_ood(probs, threshold=0.8):
+    return max(probs) < threshold 
